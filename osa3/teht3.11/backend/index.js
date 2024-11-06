4cf9c1436446d74d7e3b176de1b67a0f3d12c929
JoonasTMS86 <joonaslindberg@hotmail.com>
2024-11-06 16:23:11 +0200

    Add files via upload

---
+++ osa3/teht3.11/backend/index.js
@@ -0,0 +1,99 @@
+const express = require('express')
+const app = express()
+var morgan = require('morgan')
+
+app.use(express.json())
+
+morgan.token('body', (req, res) => JSON.stringify(req.body))
+app.use(morgan(':method :url :status :req[content-length] - :response-time ms :body'))
+
+const cors = require('cors')
+app.use(cors())
+app.use(express.static('dist'))
+
+let persons = [
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456"
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523"
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345"
+  },
+  {
+    id: 4,
+    name: "Mary Poppendick",
+    number: "39-23-6423122"
+  }
+]
+
+app.get('/', (request, response) => {
+    response.send('<h1>Hello World!</h1>')
+  })
+
+  app.get('/info', (request, response) => {
+    const txt = "Phonebook has info for " + persons.length + " people<br/><br/>" + Date()
+    response.send(txt)
+  })
+  
+  app.get('/api/persons', (request, response) => {
+    response.json(persons)
+  })
+
+  app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+    if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+  })
+
+  app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id)
+    response.status(204).end()
+  })
+
+  app.post('/api/persons', (request, response) => {
+  
+    const person = request.body
+
+    const names = persons.map(person => person.name)
+
+    if (names.includes(person.name)) {
+        return response.status(400).json({ 
+            error: 'name must be unique'
+          })
+      }
+
+    if (!person.name) {
+        return response.status(400).json({ 
+          error: 'name missing' 
+        })
+      }
+      if (!person.number) {
+        return response.status(400).json({ 
+          error: 'number missing' 
+        })
+      }
+
+    person.id = Math.floor(Math.random() * 100000)
+  
+    persons = persons.concat(person)
+  
+    response.json(person)
+  })
+
+  const PORT = 3001
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+  })
