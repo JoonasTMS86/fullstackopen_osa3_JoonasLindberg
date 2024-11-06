fe9bf277a0819f856f0b75648169a6c099309fb2
JoonasTMS86 <joonaslindberg@hotmail.com>
2024-11-06 16:24:46 +0200

    Add files via upload

---
+++ osa3/teht3.14/backend/index.js
@@ -0,0 +1,62 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+var morgan = require('morgan')
+
+app.use(express.json())
+
+morgan.token('body', (req, res) => JSON.stringify(req.body))
+app.use(morgan(':method :url :status :req[content-length] - :response-time ms :body'))
+
+const cors = require('cors')
+app.use(cors())
+app.use(express.static('dist'))
+
+const PORT = process.env.PORT
+
+const Person = require('./models/person')
+
+app.get('/', (request, response) => {
+    response.send('<h1>Hello World!</h1>')
+  })
+
+  app.get('/info', (request, response) => {
+    const txt = "Phonebook has info for " + persons.length + " people<br/><br/>" + Date()
+    response.send(txt)
+  })
+  
+  app.get('/api/persons', (request, response) => {
+    Person.find({}).then(persons => {
+      response.json(persons)
+    })
+  })
+
+  app.get('/api/persons/:id', (request, response) => {
+    Person.findById(request.params.id).then(persons => {
+      response.json(persons)
+    })
+  })
+
+  app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id)
+    response.status(204).end()
+  })
+
+  app.post('/api/persons', (request, response) => {
+    const personData = request.body
+
+    const person = new Person({
+      name: personData.name,
+      number: personData.number,
+    })
+
+    person.save().then(result => {
+    console.log('added number')
+  })
+  response.json(personData)
+})
+
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+  })
